f88dd098fc7e954d8e9f2cdf38cf3bb5acb41d1b
fanyiyang <fanyiyang@pku.edu.cn>
2022-11-26 15:24:56 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -25,8 +25,8 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 'default',
-  defaultPageCover: 'default',
+  defaultPageIcon: null,
+  defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
